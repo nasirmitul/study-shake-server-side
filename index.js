e1e7fc9a7a116e6199e22128a93d49ee87adf6c2
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-10-25 22:43:25 +0600

    courses data loaded

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) =>{
     res.send('API is running');
 });
 
-
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 app.listen(port, () => {
     console.log('Study Shake Server is Running');
