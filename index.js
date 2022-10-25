21ce12d1cbbfc92ee58d80ba53930b5650ed6a58
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-10-25 22:41:48 +0600

    server is created

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.get('/', (req, res) =>{
+    res.send('API is running');
+});
+
+
+
+app.listen(port, () => {
+    console.log('Study Shake Server is Running');
+})
