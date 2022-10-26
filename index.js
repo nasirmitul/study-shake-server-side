0d07a76e0855420c973be6319d5d5a09b6a39670
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-10-26 10:43:59 +0600

    generated api for single couese

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) =>{
     res.send('API is running');
@@ -13,6 +14,13 @@ app.get('/', (req, res) =>{
 
 app.get('/courses', (req, res) => {
     res.send(courses)
+})
+
+app.get('/courses/course/:id', (req, res) => {
+    const single_course_id = req.params.id;
+    // console.log(id);
+    const selectedCourse = course.find( c => c.course_id === single_course_id)
+    res.send(selectedCourse)
 })
 
 app.listen(port, () => {
